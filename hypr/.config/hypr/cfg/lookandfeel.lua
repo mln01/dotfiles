436ebb4c7d3df99b6d4f014d81893b5e9db1fded
mln01 <124874297+mln01@users.noreply.github.com>
2026-08-14 19:03:54 +0200

    add nvim and hypr and waybar little update

--- hypr/.config/hypr/cfg/lookandfeel.lua
+++ hypr/.config/hypr/cfg/lookandfeel.lua
@@ -1,9 +1,9 @@
 hl.config({
     general = {
-        gaps_in          = 3,
-        gaps_out         = 3,
+        gaps_in          = 8,
+        gaps_out         = 8,
 
-        border_size      = 3,
+        border_size      = 4,
 
         col              = {
             active_border = blue,
---
+++ nvim/.config/nvim/lua/themes/catppuccin-frappe.lua
@@ -0,0 +1,70 @@
+local M = {}
+
+M.base_30 = {
+  white = "#c6d0f5",
+
+  darker_black = "#292c3c",
+  black = "#303446",
+  black2 = "#292c3c",
+
+  one_bg = "#414559",
+  one_bg2 = "#51576d",
+  one_bg3 = "#626880",
+
+  grey = "#737994",
+  grey_fg = "#838ba7",
+  grey_fg2 = "#949cbb",
+  light_grey = "#a5adce",
+
+  red = "#e78284",
+  baby_pink = "#f4b8e4",
+  pink = "#f4b8e4",
+
+  line = "#414559",
+
+  green = "#a6d189",
+  vibrant_green = "#a6d189",
+
+  nord_blue = "#8caaee",
+  blue = "#8caaee",
+
+  yellow = "#e5c890",
+  sun = "#e5c890",
+
+  purple = "#ca9ee6",
+  dark_purple = "#babbdf",
+
+  teal = "#81c8be",
+  cyan = "#81c8be",
+  orange = "#ef9f76",
+
+  statusline_bg = "#292c3c",
+  lightbg = "#51576d",
+
+  pmenu_bg = "#8caaee",
+  folder_bg = "#8caaee",
+}
+
+M.base_16 = {
+  base00 = "#303446",
+  base01 = "#292c3c",
+  base02 = "#414559",
+  base03 = "#626880",
+  base04 = "#838ba7",
+  base05 = "#a5adce",
+  base06 = "#c6d0f5",
+  base07 = "#f2d5cf",
+
+  base08 = "#e78284",
+  base09 = "#ef9f76",
+  base0A = "#e5c890",
+  base0B = "#a6d189",
+  base0C = "#81c8be",
+  base0D = "#8caaee",
+  base0E = "#ca9ee6",
+  base0F = "#ea999c",
+}
+
+M.type = "dark"
+
+return M
